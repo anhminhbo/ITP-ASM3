7ee548c66ab13b53f5de37355a47ce1698954545
Khuong <s3924577@rmit.edu.vn>
2021-12-27 17:29:36 +0700

    feature 5

---
+++ features/feature5.py
@@ -0,0 +1,25 @@
+class customer:
+    def __init__(self, name, email_address, shipping_address, phone_number):
+        self.name = name
+        self.email_address = email_address
+        self.shipping_address = shipping_address
+        self.phone_number = phone_number
+
+    def show_customer_info(self):
+        print(f"""
+        Name: {self.name}
+        Email address: {self.email_address}
+        Shipping address: {self.shipping_address}
+        Phone number: {self.phone_number}
+        """)
+
+
+customers = [customer("Minh", "s3931605@rmit.edu.vn", "ABCS", "9334378563485")]
+
+
+def feature_5(customer_list):
+    for i in customer_list:
+        i.show_customer_info()
+
+
+feature_5(customers)

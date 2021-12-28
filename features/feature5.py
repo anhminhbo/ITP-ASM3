ba2ef43fa7e6385058d0d50d5c528a6479bbaea2
Khuong <s3924577@rmit.edu.vn>
2021-12-28 23:16:54 +0700

    fix feature 5 and add feature 6

--- features/feature5.py
+++ features/feature5.py
@@ -1,5 +1,9 @@
 class Customer:
+    customer_counter = 1
+
     def __init__(self, name, email_address, shipping_address, phone_number):
+        self.id = Customer.customer_counter
+        Customer.customer_counter += 1
         self.name = name
         self.email_address = email_address
         self.shipping_address = shipping_address
@@ -7,16 +11,22 @@ class Customer:
 
     def show_customer_info(self):
         print(f"""
+        Customer id: {self.id}
         Name: {self.name}
         Email address: {self.email_address}
         Shipping address: {self.shipping_address}
-        Phone number: {self.phone_number}
-        """)
+        Phone number: {self.phone_number}""")
 
 
-customers = [Customer("Minh", "s3931605@rmit.edu.vn", "ABCS", "9334378563485")]
+customers = [
+    Customer("Minh", "s3931605@rmit.edu.vn", "ABCS", "9334378563485"),
+    Customer("Minh", "s3931605@rmit.edu.vn", "ABCS", "9334378563485")
+]
 
 
 def feature_5(customer_list):
     for i in customer_list:
         i.show_customer_info()
+
+
+feature_5(customers)

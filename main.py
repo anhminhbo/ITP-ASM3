9edd57bb90033fab50b4e261790137cfaeb90ff9
Khuong <s3924577@rmit.edu.vn>
2022-01-07 10:21:41 +0700

    fix col width

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import feature6 as feature6
 import feature8 as feature8
 import feature10 as feature10
 
+
 # Set up program
 
 # Initialize shop item class
@@ -35,11 +36,11 @@ list_shop_items = [
     ShopItem('Gao phone', 'killer red limited edition'),
     ShopItem('Orga driver', 'black on gold'),
     ShopItem('Gao phone', 'victory gold'),
-    ShopItem('Samsung Galaxy S21 Ultra','black',45000000),
+    ShopItem('Samsung Galaxy S21 Ultra', 'black', 45000000),
     ShopItem('iPhone 13 Pro Max', 'blue', 67000000),
     ShopItem('Samsung Galaxy S21 Plus', 'black', 75000000),
-    ShopItem('Nokia rock-smasher 1100', 'black', 999999999999),
-    ShopItem('iPhone 6 Falcon Supernova Pink Diamond', 'gold', 123456789987654321),
+    ShopItem('Nokia rock-smasher 1100', 'black', 99999999),
+    ShopItem('iPhone 6 Falcon Supernova Pink Diamond', 'gold', 12345678),
 
 ]
 
